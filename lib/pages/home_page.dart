8c2a4ce7f486603a331e584dcfcf38d7bcc3c6b7
WuHaiheng <wuhh@csco.com.cn>
2023-06-19 16:39:16 +0800

    基于login_sdk完成首页和登陆功能调试

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:login_sdk/dao/login_dao.dart';
+import 'package:login_sdk/util/navigator_util.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  get _logoutBtn => ElevatedButton(
+      onPressed: () {
+        LoginDao.logout();
+      },
+      child: const Text('登出'));
+
+  get _listView => ListView(
+        children: [
+          _logoutBtn,
+          const Text(
+            '首页',
+            style: TextStyle(fontSize: 30, color: Colors.deepOrangeAccent),
+          )
+        ],
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    // 更新导航器上下文context
+    NavigatorUtil.updateContext(context);
+    return Scaffold(
+      appBar: AppBar(title: const Text('ChatGPT')),
+      body: _listView,
+    );
+  }
+}
