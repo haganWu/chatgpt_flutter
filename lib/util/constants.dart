591a220afd948f7810ebb6b7bda6d20befa59455
WuHaiheng <wuhh@csco.com.cn>
2023-07-05 16:45:14 +0800

    全局常量抽取

---
+++ lib/util/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  // 提问者信息
+  static String senderIcon = 'https://o.devio.org/images/o_as/avatar/tx4.jpeg';
+  static String senderName = 'HaganWu';
+  // ChatGPT信息
+  static String chatGPTIcon = 'https://o.devio.org/images/o_as/avatar/tx2.jpeg';
+  static String chatGPTName = 'ChatGPT';
+}
